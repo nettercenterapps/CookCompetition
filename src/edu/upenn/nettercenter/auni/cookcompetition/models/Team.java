b3321090d2bdb7dadaa217340fb02e872cbc6285
Siyu Song <siyu@siyusong.com>
2013-10-18 10:21:44 -0400

    add team object

---
+++ src/edu/upenn/nettercenter/auni/cookcompetition/models/Team.java
@@ -0,0 +1,24 @@
+package edu.upenn.nettercenter.auni.cookcompetition.models;
+
+import com.j256.ormlite.field.DatabaseField;
+import com.j256.ormlite.table.DatabaseTable;
+
+@DatabaseTable
+public class Team {
+	@DatabaseField(generatedId = true)
+	private Long id;
+	@DatabaseField
+	private String name;
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
